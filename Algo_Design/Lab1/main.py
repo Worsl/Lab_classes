1c5ad2866da6fbe26295eae48234726db9f1b6d1
cheng <qianchengx99@gmail.com>
2023-02-08 19:36:44 +0800

    added pretty tables, and data representation

--- Algo_Design/Lab1/main.py
+++ Algo_Design/Lab1/main.py
@@ -1,7 +1,11 @@
 import random
 import time
+from prettytable import PrettyTable
+#import mathplotlib.pyplot as plt
 
-comparisons = 0
+KEY_COMPARISONS = 0
+records = {}
+table = PrettyTable(["Value of S", "Size of array (N)","Number Of Key Comparisons"])
 
 def generate_random_array(lower_limit, upper_limit):
     arr = []
@@ -10,7 +14,7 @@ def generate_random_array(lower_limit, upper_limit):
         arr.append(random.randint(1,upper_limit))
     return arr
 
-def hybrid_sort(arr, S:int):
+def hybrid_sort(arr, S:int): #returns an array sorted
     length = len(arr)
     
     if length <= S: #base case to enter insertion sort
@@ -18,65 +22,81 @@ def hybrid_sort(arr, S:int):
         return sorted
     
     mid = length//2
-    left = arr[:mid]
-    right = arr[mid:]
+    left_subarray = arr[:mid] #uptill and exclusive of mid index
+    right_subarray = arr[mid:] #from mid index inclusive, to the end.
     
-    hybrid_sort(left, S)
-    hybrid_sort(right, S)
-    merge(left, right, arr)
-    
+    left = hybrid_sort(left_subarray,S)
+    right = hybrid_sort(right_subarray,S)
+
+
+    return merge(left,right)    
+  
 def insertion_sort(arr):
-    global comparisons
+    global KEY_COMPARISONS
     length = len(arr)
     for i in range(1, length):
         for j in range(i,0,-1):
-            comparisons += 1
+            KEY_COMPARISONS += 1
             if arr[j-1] > arr[j]:
                 arr[j-1],arr[j] = arr[j],arr[j-1] #Python swap syntax
             else:
                 break
     return arr
 
-def merge(left, right, arr):
-    global comparisons # need to implement the comparison for part (c)
+def merge(left, right):
+    global KEY_COMPARISONS # need to implement the comparison for part (c)
     
-    i = j = k = 0
-    while i < len(left) and j < len(right): # merge requires changing into the NTU algorithm perhaps
-        if left[i] < right[j]:
-            arr[k] = left[i]
-            i += 1
+    sorted_array = [] #using an external axuiliary array
+    leftIndex,rightIndex = 0,0
+    
+    while (leftIndex < len(left) or rightIndex < len(right)):
+        if leftIndex == len(left):
+            sorted_array.append(right[rightIndex]);
+            rightIndex += 1
+        elif rightIndex == len(right):
+            sorted_array.append(left[leftIndex])
+            leftIndex += 1
+            
+        elif (left[leftIndex] < right[rightIndex]):
+            sorted_array.append(left[leftIndex])
+            leftIndex += 1
+            KEY_COMPARISONS += 1
         else:
-            arr[k] = right[j]
-            j += 1
-        k += 1
-    while i < len(left):
-        arr[k] = left[i]
-        i += 1
-        k += 1
-    while j < len(right):
-        arr[k] = right[j]
-        j += 1
-        k += 1
-
+            sorted_array.append(right[rightIndex]);
+            rightIndex += 1
+            KEY_COMPARISONS += 1
+            
+    return sorted_array
+    
 def pause():
     time.sleep(1)
 
+
+
 def main():
-    i = 5
-    while (i > 0):
-        print("random array generated..")
-        arr = generate_random_array(5,100)
-        print(arr)
+    global KEY_COMPARISONS
+    
+    S = int(input("enter value of S: "))
+    size_of_array = 10
+    
+    while size_of_array <= 5000:
+        KEY_COMPARISONS = 0
+        print(f"random array generated..",end="\n")
+        arr = generate_random_array(S,size_of_array) 
+        print(arr,end="\n")
+
         
-        pause()
-        print("sorting the array...")
-        pause()
-        hybrid_sort(arr,7)
-        print(arr,end='\n')
+        print("sorting the array...",end="\n")
+        print(hybrid_sort(arr,S),end="\n")
+
         
-        print()
-        pause()
+        #print(f"value of S : {S}, number of key comparisons : {KEY_COMPARISONS} , size of array : {size_of_array}", end="\n" )
+        table.add_row([S,size_of_array,KEY_COMPARISONS])
         
-        i -= 1
+        size_of_array +=10
+        
+        
+
         
 main()
+print(table)
